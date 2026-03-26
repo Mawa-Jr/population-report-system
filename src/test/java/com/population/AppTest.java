a9b6d178c89ad018d7c6292561b916b41b46fe8b
Mawa-Jr <mauricio.yamjr@gmail.com>
2026-03-25 20:53:00 -0600

    Add Report 1: All countries by population

--- src/test/java/com/population/AppTest.java
+++ src/test/java/com/population/AppTest.java
@@ -6,30 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
 
     @Test
-    public void testDatabaseConnection() {
-        // Test will pass if database connection works
-        assertTrue(true, "Basic test passed");
-    }
-
-    @Test
     public void testGetTotalCountries() {
-        // Test that getTotalCountries returns a number
         int total = App.getTotalCountries();
         assertTrue(total > 0, "Total countries should be greater than 0");
     }
 
     @Test
-    public void testGetTotalCities() {
-        // Test that getTotalCities returns a number
-        int total = App.getTotalCities();
-        assertTrue(total > 0, "Total cities should be greater than 0");
-    }
-
-    @Test
     public void testGetAllCountriesByPopulation() {
-        // This just verifies the method runs without exception
         assertDoesNotThrow(() -> App.getAllCountriesByPopulation());
     }
-
 }
-
