6a18561d2145992c9e7fc40df81ec6337e02565f
Mawa-Jr <mauricio.yamjr@gmail.com>
2026-03-30 16:38:22 -0600

    Update tests for Report 4

--- src/test/java/com/population/AppTest.java
+++ src/test/java/com/population/AppTest.java
@@ -6,18 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
 
     @Test
+    public void testDatabaseConnection() {
+        assertTrue(true, "Basic test passed");
+    }
+
+    @Test
     public void testGetTotalCountries() {
         int total = App.getTotalCountries();
         assertTrue(total > 0, "Total countries should be greater than 0");
     }
+
     @Test
-    public void testGetTotalCities() {
-        // Test that getTotalCities returns a number
-        int total = App.getTotalCities();
-        assertTrue(total > 0, "Total cities should be greater than 0");
-    }
-        @Test
     public void testGetAllCountriesByPopulation() {
         assertDoesNotThrow(() -> App.getAllCountriesByPopulation());
+    }
+
+    @Test
+    public void testGetTopNCitiesByPopulation() {
+        assertDoesNotThrow(() -> App.getTopNCitiesByPopulation());
     }
 }
